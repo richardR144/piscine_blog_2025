7561c06ba661ae9652b138ef7359b6ce006af1b0
richardR144 <richard.rigollet@lapiscine.pro>
2025-05-02 16:38:09 +0200

    create-article avec la méthode POST

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,13 +4,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends AbstractController
 {
     #[Route('/create-article', name: 'create-article')]
-    public function displayCreateArticle(): Response
+    public function displayCreateArticle(Request $request): Response
     {
+        if($request->isMethod('POST')) {
+            $title = $request->request->get('title');
+            $content = $request->request->get('content');
+            $description = $request->request->get('description');
+            $image = $request->files->get('image');
+
+            
+          
+        }
         return $this->render('create-article.html.twig');
     }
 }
+        dump($title); 
+        dump($content);
+        dump($description);
+        dump($image);
+        die;
+    
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -6,8 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 
 class PageController extends AbstractController
@@ -15,24 +13,8 @@ class PageController extends AbstractController
     #[Route('/', name: 'home')]
     public function displayHome(): Response
     {
-        $article = new Article();
-        $article->setTitle('')
-            ->setDescription('')
-            ->setContent('This is the content of my first article')
-            ->setImage('image.jpg')
-            ->setCreatedAt(new \DateTime())
-            ->setIsPublished(true);
-
-            return $this->render('home/index.html.twig', [
-                'article' => $article,
-            ]);
-
-                //return new Response('Article created with id ' . $article->getId());
-    }
-}
-{
-   
+         return $this->render('home.html.twig');
+    }  
     
 }
 
-//dd('hello');
